274873edbaf66e54c2e162c4e01e7596c2ae7dff
Jenny ☺ <jenny@jennyrobertson.net>
2020-04-13 18:25:31 +0100

    calculate spool status

--- src/items/Spool.js
+++ src/items/Spool.js
@@ -11,34 +11,48 @@ export class Spool {
         this.spoolAngle = 0;
         this.meterSpeed = 5;
 
-        this.spoolSprite = getSprite('spool');
+        // How many degrees it extends to on each side of 0
+        this.meterGreenArea = 45;
+        this.meterYellowArea = 65;
+
+        this.statuses = {
+            green: 'GREEN',
+            yellow: 'YELLOW',
+            red: 'RED',
+            idle: 'IDLE'
+        }
+        this.status = this.statuses.idle;
+
         this.meterSprite = getSprite('spool-meter');
-        this.meterOutlineSprite = getSprite('spool-meter-outline');
+        this.spoolSprite = getSprite('spool');
+        // this.meterOutlineSprite = getSprite('spool-meter-outline');
 
         this.spoolDiameter = 51;
         this.meterDiameter = 65;
-        this.drawDiff = (this.meterDiameter - this.spoolDiameter ) * 0.5; // 6
+        this.meterThickness = (this.meterDiameter - this.spoolDiameter ) * 0.5;
 
     }
+
     draw({cursorX, cursorY}) {
         this.drawMeter();
         this.drawSpool({cursorX, cursorY});
         this.meterAngle = (this.meterAngle + this.meterSpeed) % 360;
-
-        console.log(this.spoolAngle);
+        this.status = this.getStatus();
+        console.log(this.status);
     }
+
     drawSpool({cursorX, cursorY}) {
         this.spoolAngle = this.getAngleFromMouse({
-            centerX: (this.x + this.drawDiff + (this.spoolDiameter * 0.5)) * STORE.increase, 
-            centerY: (this.y + this.drawDiff + (this.spoolDiameter * 0.5)) * STORE.increase, 
+            centerX: (this.x + this.meterThickness + (this.spoolDiameter * 0.5)) * STORE.increase, 
+            centerY: (this.y + this.meterThickness + (this.spoolDiameter * 0.5)) * STORE.increase, 
             cursorX,
             cursorY
         });
  
         drawAssetRotated(STORE.ctx, {
             spriteSheet: this.spoolSprite,
-            x: this.x + this.drawDiff,
-            y: this.y + this.drawDiff,
+            x: this.x + this.meterThickness,
+            y: this.y + this.meterThickness,
             width: this.spoolDiameter,
             height: this.spoolDiameter,
             localCenterX: this.spoolDiameter * 0.5,
@@ -47,6 +61,7 @@ export class Spool {
         });
 
     }
+
     drawMeter() {
         drawAssetRotated(STORE.ctx, {
             spriteSheet: this.meterSprite,
@@ -61,6 +76,8 @@ export class Spool {
 
         // drawAsset(STORE.ctx, {spriteSheet: this.meterOutlineSprite, x: this.x - 3, y: this.y - 3});
     }
+
+    /* Returns how much spool should rotate based on mouse position */
     getAngleFromMouse({centerX, centerY, cursorX, cursorY}) {
         // make center of circle 0
         const x = cursorX - centerX;
@@ -72,5 +89,17 @@ export class Spool {
         const value = (Math.ceil(angle / roundAngleTo) * roundAngleTo) + 90;
         // was going from 270 to -85 :(
         return value < 0 ? value + 360 : value;
+    }
+
+    /* Returns if knob is in green, yellow or red area */
+    getStatus() {
+        // remove differences for rotation
+        const spoolVsMeterDiff = this.spoolAngle - this.meterAngle;
+        // get angle diff as degrees either side of zero, e.g. -15 or +30
+        const angleDiff = spoolVsMeterDiff > 180 ? spoolVsMeterDiff - 360 : spoolVsMeterDiff;
+
+        if (angleDiff > -this.meterGreenArea && angleDiff < this.meterGreenArea) return this.statuses.green;
+        if (angleDiff > -this.meterYellowArea && angleDiff < this.meterYellowArea) return this.statuses.yellow;
+        return this.statuses.red;
     }
 }
